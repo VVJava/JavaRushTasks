5c782090f83e8029702105b30e294b03fb7ba3a7
VVJava <javacode877@gmail.com>
2024-05-12 23:32:08 +0300

    Add Solution Java Syntax task0416

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task04.task0416;
+
+import java.util.Scanner;
+
+/* 
+Share a Coke
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       Scanner keyboard = new Scanner(System.in);
+       System.out.println("Количество банок колы в ящике: ");
+       int kola = keyboard.nextInt();
+       System.out.println("Количество сотрудников в кабинете: ");
+       int sotrudnik = keyboard.nextInt();
+       double sum = 1.*kola/sotrudnik ;
+       System.out.println("Каждому сотруднику достанется банок колы: ");
+       System.out.println(sum);
+
+
+
+    }
+}

171cc5557361c095ae1c242e7a21add81f2a6a71
VVJava <javacode877@gmail.com>
2024-07-01 18:03:12 +0300

    Add Solution Java Syntax task0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+   int number;
+   String String;
+   double aDouble;
+   float aFloat;
+   char aChar;
+   short aShort;
+   byte aByte;
+   boolean aBoolean;
+   long aLong;
+}

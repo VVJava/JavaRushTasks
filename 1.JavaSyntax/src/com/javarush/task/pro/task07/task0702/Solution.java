1d0c4260f9697564621756893351a15f3de667c2
VVJava <javacode877@gmail.com>
2024-07-01 18:03:00 +0300

    Add Solution Java Syntax task0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    int number = Integer.MAX_VALUE;
+    byte aByte = Byte.MAX_VALUE;
+    char aChar = Character.MAX_VALUE;
+    long aLong = Long.MAX_VALUE;
+    short aShort = Short.MAX_VALUE;
+}

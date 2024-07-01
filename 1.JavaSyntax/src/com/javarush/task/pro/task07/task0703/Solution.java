40ba8d70cb821cc539dbf659bfd71646019485aa
VVJava <javacode877@gmail.com>
2024-07-01 18:05:38 +0300

    Add Solution Java Syntax task0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte aByte = Byte.MIN_VALUE;
+    int anInt = Integer.MIN_VALUE;
+    short aShort = Short.MIN_VALUE;
+    long aLong = Long.MIN_VALUE;
+    char aChar = Character.MIN_VALUE;
+
+}
